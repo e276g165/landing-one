4a39e2f3c6f1841100cbfc11fcee5f47b596d0b9
Elijah Gabriel <egabrie2@stumail.jccc.edu>
2022-05-18 10:55:19 -0500

    Adding animations

---
+++ source.js
@@ -0,0 +1,28 @@
+// @Author - Elijah Gabriel
+
+function typing () {
+    let text = ["Build", "Your", "Dream", "Website", "Today"];
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = '';
+
+    (function type(){
+
+        if (count === text.length) {
+            count = 0;
+        }
+        currentText = text[count];
+        letter = currentText.slice(0, ++index)
+        document.querySelector('.typing').textContent = letter;
+
+        if(letter.length === currentText.length) {
+            count++;
+            index = 0;
+        }
+        setTimeout(type, 150);
+
+    }());
+}
+
+typing();
